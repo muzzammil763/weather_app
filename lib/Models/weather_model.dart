34c92266d5772d75aac634ced710977cde260377
muzzammil763 <deadbase763@gmail.com>
2024-02-21 14:03:38 +0500

    added weather model class

---
+++ lib/Models/weather_model.dart
@@ -0,0 +1,25 @@
+class WeatherModel {
+  String? cityName;
+  double? temp;
+  double? wind;
+  int? humidity;
+  int? pressure;
+  double? feelsLike;
+
+  WeatherModel(
+      {this.cityName,
+      this.temp,
+      this.wind,
+      this.humidity,
+      this.pressure,
+      this.feelsLike});
+
+  WeatherModel.fromJson(Map<String, dynamic> json) {
+    cityName = json['name'];
+    temp = json['main']['temp'];
+    wind = json['wind']['speed'];
+    humidity = json['main']['humidity'];
+    pressure = json['main']['pressure'];
+    feelsLike = json['main']['feels_like'];
+  }
+}
