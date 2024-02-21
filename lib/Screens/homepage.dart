452a6f8a189415b7d9f671de68382eb8ca97f3ca
muzzammil763 <deadbase763@gmail.com>
2024-02-21 12:06:20 +0500

    added a customized widget

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/Widgets/curent_weather.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -6,16 +7,29 @@ class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffffeff6),
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent[700],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xffffeff6),
         title: const Text(
-          'Homepage',
+          'Weather App',
           style: TextStyle(
             fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+            color: Colors.black,
           ),
         ),
+      ),
+      body: const Column(
+        children: [],
       ),
     );
   }
---
+++ lib/Widgets/curent_weather.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Widget currentWeather(IconData icon, String temp, String location) {
+  return Center(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: Colors.orange,
+          size: 65,
+        ),
+      ],
+    ),
+  );
+}
