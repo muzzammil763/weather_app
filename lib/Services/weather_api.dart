47684b9d7d533ae8dea4bd8ab7869056bef2b8de
muzzammil763 <deadbase763@gmail.com>
2024-02-22 20:14:14 +0500

    added dotenv

--- lib/Services/weather_api.dart
+++ lib/Services/weather_api.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/Models/weather_model.dart';
 
 class WeatherApi {
+  final apiKey = dotenv.env['VAR_NAME'];
   Future<WeatherModel>? getCurrentWeather(String? location) async {
     var endpoint = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=eb42031611601a1401c1e7423f7ce112&units=metric');
+        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey&units=metric');
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
     return WeatherModel.fromJson(body);
