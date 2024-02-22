17a2fc0a129a45cffd54ca088313e66ad4fe5055
muzzammil763 <deadbase763@gmail.com>
2024-02-22 14:12:44 +0500

    added location selecter screen

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/Models/weather_model.dart';
+import 'package:weather_app/Screens/location_selecter.dart';
 import 'package:weather_app/Widgets/additional_information.dart';
 import 'package:weather_app/Widgets/current_weather.dart';
 import 'package:weather_app/Services/weather_api.dart';
@@ -25,6 +26,20 @@ class _HomePageScreenState extends State<HomePageScreen> {
     return Scaffold(
       backgroundColor: const Color(0xffe7ffe6),
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LocationSelecter()));
+            },
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+        ],
         centerTitle: true,
         backgroundColor: const Color(0xffd3ffd2),
         title: const Text(
@@ -39,33 +54,45 @@ class _HomePageScreenState extends State<HomePageScreen> {
         future: getData(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return ListView(
-              children: [
-                Column(
-                  children: [
-                    const SizedBox(height: 32),
-                    currentWeather(
-                      Icons.wb_sunny_rounded,
-                      '${data?.temp}',
-                      '${data?.cityName}',
-                    ),
-                    const SizedBox(height: 12),
-                    const Divider(indent: 100, endIndent: 100, thickness: 1),
-                    const SizedBox(height: 16),
-                    Text(
-                      "Additional Information",
-                      style: GoogleFonts.inter(
-                        fontSize: 20,
+            return RefreshIndicator(
+              color: Colors.black,
+              backgroundColor: const Color(0xffd3ffd2),
+              onRefresh: () {
+                getData();
+                setState(() {});
+                return Future.value();
+              },
+              child: ListView(
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: 32),
+                      currentWeather(
+                        Icons.wb_sunny_rounded,
+                        '${data?.temp}',
+                        '${data?.cityName}',
                       ),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    additionalInformation("${data?.wind}", "${data?.humidity}",
-                        "${data?.pressure}", "${data?.feelsLike}"),
-                  ],
-                ),
-              ],
+                      const SizedBox(height: 12),
+                      const Divider(indent: 100, endIndent: 100, thickness: 1),
+                      const SizedBox(height: 16),
+                      Text(
+                        "Additional Information",
+                        style: GoogleFonts.inter(
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      additionalInformation(
+                          "${data?.wind}",
+                          "${data?.humidity}",
+                          "${data?.pressure}",
+                          "${data?.feelsLike}"),
+                    ],
+                  ),
+                ],
+              ),
             );
           } else if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
---
+++ lib/Screens/location_selecter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationSelecter extends StatelessWidget {
+  const LocationSelecter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
